fe2e8b67cd1ebe473e9110b58e6a9dc24e19643f
Ian Remillard <Doom-Mac@Ians-iMac.local>
2017-03-03 20:34:07 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Instabot::Application.config.secret_key_base = 'cd18c97152ace6b2ad61cc42ef7ac1bd60437bb381fa0198668224e7a7df9ea12acb5894ea7050a8a7ecca99f6db3fd94cd761dd5856db35c29d0aed4da83955'
